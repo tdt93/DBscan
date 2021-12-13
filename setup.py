c67166faa98ac53d0a416b18c4a91df11e598a0e
tdt93 <tdthang93@gmail.com>
2021-12-13 10:37:09 +0100

    changes to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ from setuptools import setup
 
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        setup(use_scm_version={"version_scheme": "no-guess-dev"},
+              install_requires=['scikit-learn', 'matplotlib', 'numpy'],
+              author="Thang Trinh Duy",
+              license="MIT"
+              )
+
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
